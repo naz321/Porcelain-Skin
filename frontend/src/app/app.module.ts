4fcc7be62bbeaabdeccdf43a0aaabf768c6a92d8
Mo Haider <mo.haider50@gmail.com>
2020-04-27 17:22:04 -0400

    tested frontend setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent
   ],
   imports: [
     BrowserModule,
