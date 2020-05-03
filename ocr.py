d5e908140f66d5c98c398671c6834a678f2d6288
Naziba Haider <haidernaziba@gmail.com>
2020-05-03 13:00:11 -0400

    created pytesseract script to convert image to text

---
+++ ocr.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import cv2 
+import sys
+import pytesseract
+import argparse
+import os
+
+
+
+ap = argparse.ArgumentParser()
+# --image is the path to the image we are sending through the OCR system
+ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
+#ap.add_argument("-p", "--preprocess", type=str, default="blur", help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+# converting the image from its current color space to a gray color space
+src = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# TODO: spelling checks (e.g. "l is often unidentified")
+# TODO: zoom in images before processing them as results are more accurate when text is bigger
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, src)
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+
